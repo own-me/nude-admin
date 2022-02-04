675f342198f949077613aa4af24b59e6e3030741
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-02-03 22:17:39 -0800

    add MUI packages

--- Main.tsx
+++ Main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Button from "@mui/material/Button";
 
 export default function Main() {
     return (
         <div id="main-container">
             Admin App
+            <Button variant="contained">Hello World</Button>
         </div>
     );
 }
