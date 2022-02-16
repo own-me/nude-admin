38aa7f315f51b099cee81fc1d298fab6cdb0ef8b
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-02-15 22:53:53 -0800

    add nft API to store

--- redux/store.tsx
+++ redux/store.tsx
@@ -1,11 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import appReducer from "./slices/app";
+import { nftApi } from "./api/nft";
 
 export const store = configureStore({
     reducer: {
         app: appReducer,
-    }
+        [nftApi.reducerPath]: nftApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
+        nftApi.middleware
+    ),
 });
 
 setupListeners(store.dispatch);
