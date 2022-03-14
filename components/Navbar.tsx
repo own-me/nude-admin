45517e9b75cab19e982fd63819ba14eb49a51352
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-03-13 19:22:43 -0700

    remove bad image import

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -166,7 +166,7 @@ const Navbar = memo(() => {
                     <Box sx={{ flexGrow: 0 }}>
                         <Tooltip title="Open settings">
                             <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                <Avatar alt="Account" />
                             </IconButton>
                         </Tooltip>
                         <Menu
