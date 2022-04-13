e83ab50e13ca448d68d8cc1b87b6edde9bda256f
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-04-12 22:48:09 -0700

    use HashRouter

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
@@ -15,11 +15,11 @@ const theme = createTheme({
 function App() {
     return (
         <Provider store={store}>
-            <Router>
+            <HashRouter>
                 <ThemeProvider theme={theme}>
                     <Main />
                 </ThemeProvider>
-            </Router>
+            </HashRouter>
         </Provider>
     );
 }
